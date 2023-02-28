3727664ad509484fb625caae1a3ee5a0d307f55e
Jörg Finkeisen <joerg@finkeisen.net>
2023-02-28 19:56:04 +0100

    first complete version

--- scripts/dicee.js
+++ scripts/dicee.js
@@ -10,5 +10,16 @@ const imageURLs = [
 var dice1 = Math.floor(Math.random() *6);
 var dice2 = Math.floor(Math.random() *6);
 
+document.querySelector("#dice1").src=imageURLs[dice1];
+document.querySelector("#dice2").src=imageURLs[dice2];
 
+if (dice1 > dice2) {
+    document.querySelector("h1").innerHTML="Player 1 wins!";
+}
+else if (dice2 > dice1) {
+    document.querySelector("h1").innerHTML="Player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML="No winner!";
+}
 
