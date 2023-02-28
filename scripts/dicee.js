5100d2ed357c7405c0665d4d9932784b0a56a12b
Joerg Finkeisen <joerg@finkeisen.net>
2023-02-28 19:17:20 +0100

    js started

---
+++ scripts/dicee.js
@@ -0,0 +1,14 @@
+const imageURLs = [
+    "images/dice1.png",
+    "images/dice2.png",
+    "images/dice3.png",
+    "images/dice4.png",
+    "images/dice5.png",
+    "images/dice6.png"
+];
+
+var dice1 = Math.floor(Math.random() *6);
+var dice2 = Math.floor(Math.random() *6);
+
+
+
